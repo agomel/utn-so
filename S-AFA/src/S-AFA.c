06a801bdf774dbaf17b6cefd020e252566070996
florlipchak <florencia.lipchak@gmail.com>
2018-09-01 16:47:01 -0300

    Intento de conexiones

--- S-AFA/src/S-AFA.c
+++ S-AFA/src/S-AFA.c
@@ -8,10 +8,55 @@
  ============================================================================
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <commons/log.h>
+#include <commons/collections/list.h>
 
 int main(void) {
-	puts("!!!Esto es S-AFA!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(20000);
+
+	int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+	int activado = 1;
+	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+	if(bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0){
+		perror("FALLÓ EL BIND");
+		return 1;
+	}
+
+	puts("Estoy escuchando");
+	listen(servidor, 100);
+
+	//.........................
+
+	struct sockaddr_in direccionCliente;
+	unsigned int tamanioDireccion;
+	int cliente = accept(servidor, (void*) &direccionCliente, &tamanioDireccion);
+
+	printf("Recibi una conexion en %d!!\n", cliente);
+	send(cliente, "Hola!", 6, 0);
+
+	//........................
+
+	char* buffer = malloc(strlen("Hola!"));
+
+	int bytesRecibidos = recv(cliente, (void*) &buffer, 4, 0);
+	if(bytesRecibidos < 0){
+		perror("Se desconectó el cliente.");
+		return 1;
+	}
+
+	buffer[bytesRecibidos] = '\0';
+	printf("Me llegaron %d bytes con $s", bytesRecibidos, buffer);
+
+	free(buffer);
+
+	return 0;
 }
