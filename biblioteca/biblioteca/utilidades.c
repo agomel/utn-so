64e4a4959096f8aad81c5f90d69473fbad2f7112
brendaboryszanski <brenborys@gmail.com>
2018-09-09 14:42:19 -0300

    agregados parametros

--- biblioteca/biblioteca/utilidades.c
+++ biblioteca/biblioteca/utilidades.c
@@ -2,7 +2,7 @@
 void* asignarMemoria(int cantidad){
 	void* buffer = malloc(cantidad);
 	if(buffer == NULL){
-		printf("No hay espacio");
+		printf("No hay espacio\n");
 		exit(-99);
 	}
 	return buffer;
