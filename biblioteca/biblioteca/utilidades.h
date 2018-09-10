e4e0b6c407e85a7f80b991d43494fb9d45828dd5
florlipchak <florencia.lipchak@gmail.com>
2018-09-10 17:45:42 -0300

    creado enum operaciones

--- biblioteca/biblioteca/utilidades.h
+++ biblioteca/biblioteca/utilidades.h
@@ -6,11 +6,12 @@
 #include <string.h>
 #include <malloc.h>
 
-
 #define ARCHIVO_CONFIGURACION "../configuraciones/configuracion.txt"
 #define ARCHIVO_LOG "../../log.txt"
 
-
+enum{
+	identificarse=0, mandarTexto=1
+};
 void* asignarMemoria(int cantidad);
 
 #endif /*UTILIDADES_H_*/
