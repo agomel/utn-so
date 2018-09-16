ce0a7317204f9ecded0c021a22954747ae160acd
florlipchak <florencia.lipchak@gmail.com>
2018-09-16 20:10:59 -0300

    obtener datos

--- MDJ/src/operaciones.c
+++ MDJ/src/operaciones.c
@@ -5,3 +5,9 @@ u_int32_t validarArchivo(int emisor){
 	return true;
 }
 void crearArchivo(int emisor){}
+
+void obtenerDatos(u_int32_t emisor){
+	char* rutaArchivo = deserializarString(emisor);
+	char* desplazamiento = deserializarString(emisor);
+	int tamanioALeer =
+ }
