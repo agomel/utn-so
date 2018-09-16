d520fad6d32a41a59ff123faf660afc6aa937cc3
natybekerman <natalybekerman@gmail.com>
2018-09-16 16:41:48 -0300

    FM9 conexion

--- FM9/src/FM9.c
+++ FM9/src/FM9.c
@@ -16,9 +16,35 @@
 #include <biblioteca/hilos.h>
 
 t_dictionary* conexiones;
+u_int32_t socketCPU;
+u_int32_t socketDAM;
 
 void entenderMensaje(int emisor, int header){
-	deserializarIdentificarse(emisor,conexiones);
+	char identificado;
+		switch(header){
+			case IDENTIFICARSE:
+				//TODO agregar tambien el socket identificado al mapa de conexiones
+				identificado = deserializarIdentificarse(emisor);
+				printf("identificado %c \n", identificado);
+				switch(identificado){
+					case CPU:
+						socketCPU = emisor;
+						break;
+					case DAM:
+						socketDAM = emisor;
+						break;
+					default:
+						perror("no acepto a esta conexion");
+				}
+				printf("Se agrego a las conexiones %c \n" , identificado);
+
+			case MANDAR_TEXTO:
+				//TODO esta operacion es basura, es para probar a serializacion y deserializacion
+				deserializarString(emisor);
+				break;
+			default:
+				perror("Cualquiera ese header flaco");
+		}
 }
 
 int main(void) {
@@ -31,4 +57,5 @@ int main(void) {
 	pthread_t hiloAdministradorDeConexiones = crearHilo(&escucharClientes, &parametros);
 
 	esperarHilo(hiloAdministradorDeConexiones);
+	return 0;
 }
