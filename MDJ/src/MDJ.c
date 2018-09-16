d6e746d926731cc9e289f9e9fc4297e4fd10b4e5
natybekerman <natalybekerman@gmail.com>
2018-09-16 17:35:34 -0300

    Config MDJ

--- MDJ/src/MDJ.c
+++ MDJ/src/MDJ.c
@@ -10,8 +10,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <biblioteca/utilidades.h>
+#include <biblioteca/socket.h>
+#include <biblioteca/select.h>
+#include <biblioteca/hilos.h>
+
+
+u_int32_t socketDAM;
+
+
+void escuchar(int servidor){
+	int a = 1;
+	while(a){
+		char* buffer=asignarMemoria(150);
+		int bytesRecibidos = recibirMensaje(servidor,&buffer,150);
+		if(bytesRecibidos <= 0){
+			a = 0;
+		}
+		printf("%s \n", buffer);
+		free(buffer);
+	}
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	direccionServidor direccionMDJ = levantarDeConfiguracion(NULL, "PUERTO", ARCHIVO_CONFIGURACION);
+	int servidor = crearServidor(direccionMDJ.puerto, INADDR_ANY);
+
+	socketDAM = aceptarCliente(servidor);
+	empezarAEscuchar(servidor, 1);
+	pthread_t hiloEscuchadorDAM = crearHilo(&escuchar, socketDAM);
+
+	return 0;
 }
