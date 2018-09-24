d42090dda09c36bb76179dad9955a7f860aa8702
natybekerman <natalybekerman@gmail.com>
2018-09-24 19:20:27 -0300

    agregados prints

--- S-AFA/src/plp.c
+++ S-AFA/src/plp.c
@@ -4,11 +4,14 @@ void planificadorALargoPlazo(){
 	u_int32_t a = 1;
 	while(a){
 		if(!queue_is_empty(colaNEW)){
-			printf("b\n");
 			waitMutex(&mutexNEW);
 			DTB* dtb = queue_pop(colaNEW);
+			printf("Pude sacar de la cola\n");
+			DTB dtb2 = *dtb;
+			char* m = dtb2.escriptorio;
+			printf("el escriptorio sacado de la cola es: %s\n", m);
 			signalMutex(&mutexNEW);
-			enviarDTB(*dtb);
+			enviarDTB(dtb2);
 			waitSem(&espacioDisponibleREADY);
 			waitMutex(&mutexREADY);
 			queue_push(colaREADY, dtb);
@@ -20,9 +23,12 @@ void planificadorALargoPlazo(){
 
 void ponerProcesoEnNew(char* escriptorio){
 	DTB proceso = crearDTB(escriptorio);
+	printf("el escriptorio fuera del dtb: %s\n", escriptorio);
 	waitMutex(&mutexNEW);
 	queue_push(colaNEW, &proceso);
 	signalMutex(&mutexNEW);
+	char* m = proceso.escriptorio;
+	printf("el escriptorio es: %s\n", m);
 }
 
 void enviarDTB(DTB proceso){
