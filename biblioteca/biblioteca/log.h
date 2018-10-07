d33dcc8e187ea1c382d7da3277fb9c63895549c5
agomel <agomel97@gmail.com>
2018-10-07 14:38:27 -0300

    log

---
+++ biblioteca/biblioteca/log.h
@@ -0,0 +1,12 @@
+#ifndef LOG_H_
+#define LOG_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <commons/log.h>
+
+t_log* crearLogger(char* file, char *programName);
+
+
+#endif /*LOG_H_*/
