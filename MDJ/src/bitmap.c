ce3f0f86c6a0371369011be73089d47cfd6d3978
agomel <agomel97@gmail.com>
2018-11-18 19:23:12 -0300

    funcaaa

--- MDJ/src/bitmap.c
+++ MDJ/src/bitmap.c
@@ -9,14 +9,10 @@ int cantidadBloquesBitmapEnBytes(){
 }
 
 t_bitarray* crearBitarray(){
-	unsigned char byteArray [cantidadBloquesBitmapEnBytes()];
+	char* byteArray = asignarMemoria(cantidadBloquesBitmapEnBytes());
 
 	FILE* archivoBitMap = fopen(rutaBitmap, "rb");
 	fread(byteArray, cantidadBloquesBitmapEnBytes(), 1, archivoBitMap);
-	//agregarBarraCero(byteArray);
-
-
-	//printf("el bitArray contiene: %s\n", bytesToBits(byteArray));
 
 	return bitarray_create_with_mode(byteArray, cantidadBloquesBitmapEnBytes(), MSB_FIRST);
 }
@@ -37,15 +33,18 @@ void persistirBitMap(){
 void initBitmap(){
 	rutaBitmap = concatenar(PUNTO_MONTAJE_METADATA, "Bitmap.bin");
 	bitarray = crearBitarray();
-
+	printf("INIT- la cant de bloques %d \n", bitarray_get_max_bit(bitarray));
 	printf("INIT- cant libres es %d \n", cantidadTotalDeBloquesLibres());
+	for(int i = 0; i < bitarray_get_max_bit(bitarray); i++){
+		printf("%d", bitarray_test_bit(bitarray, i));
+	}
+	printf("\n");
 }
 
 int cantidadTotalDeBloquesLibres(){
 	int cantidadLibre = 0;
 	for(int i = 0; i < bitarray_get_max_bit(bitarray); i++){
 		if(bitarray_test_bit(bitarray, i) == 0){
-			printf("el bloque %d esta libre\n", i);
 			cantidadLibre++;
 		}
 	}
@@ -66,7 +65,6 @@ int obtenerBloqueLibreBitmap(){
 	int posicion;
 	for(int i = 0; i < bitarray_get_max_bit(bitarray); i++){
 		if(bitarray_test_bit(bitarray, i) == 0){
-			printf("el bloque libre es el %d\n", i);
 			posicion = i;
 			break;
 		}
