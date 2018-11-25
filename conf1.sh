17b493fb8d40a63e912018739016f853286430e8
florlipchak <florencia.lipchak@gmail.com>
2018-11-25 20:22:39 -0300

    script de configuracionn

---
+++ conf1.sh
@@ -0,0 +1,22 @@
+cd 
+cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/DAM/prueba1
+rm ../../../DAM/Debug/configuraciones/configuracion.txt
+cp configuracion.txt ../../../DAM/Debug/configuraciones
+cd 
+cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/CPU/prueba1
+rm ../../../CPU/Debug/configuraciones/configuracion.txt
+cp configuracion.txt ../../../CPU/Debug/configuraciones
+cd 
+cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/SAFA/prueba1
+rm ../../../S-AFA/Debug/configuraciones/configuracion.txt
+cp configuracion.txt ../../../S-AFA/Debug/configuraciones
+cd 
+cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/MDJ/prueba1
+rm ../../../MDJ/Debug/configuraciones/configuracion.txt
+cp configuracion.txt ../../../MDJ/Debug/configuraciones
+cd 
+cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/FM9/prueba1
+rm ../../../FM9/Debug/configuraciones/configuracion.txt
+cp configuracion.txt ../../../FM9/Debug/configuraciones
+cd 
+cd /workspace/tp-2018-2c-5-fant-sticos
