840f0c301e0edeaecc0ffafb46c547ba8b93ead5
julisasla <juliansaslavsky@gmailcom>
2018-11-24 22:56:38 -0300

    cript

---
+++ configurarCarpetas.sh
@@ -0,0 +1,18 @@
+cd
+cd workspace/tp-2018-2c-5-fant-sticos/CPU
+cp -R configuraciones/ Debug/
+cd
+cd workspace/tp-2018-2c-5-fant-sticos/DAM
+cp -R configuraciones/ Debug/
+cd
+cd workspace/tp-2018-2c-5-fant-sticos/S-AFA
+cp -R configuraciones/ Debug/
+cd
+cd workspace/tp-2018-2c-5-fant-sticos/FM9
+cp -R configuraciones/ Debug/
+cd
+cd workspace/tp-2018-2c-5-fant-sticos/MDJ
+cp -R configuraciones/ Debug/
+cd
+cd workspace/tp-2018-2c-5-fant-sticos/MDJ
+cp -R mnt/ Debug/
