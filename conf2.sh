21297d72618ec37aa4e9c2e3a2193fe3377ec1a7
florlipchak <florencia.lipchak@gmail.com>
2018-11-25 20:28:05 -0300

    saco /

--- conf2.sh
+++ conf2.sh
@@ -1,22 +1,22 @@
 cd 
-cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/DAM/prueba2
+cd workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/DAM/prueba2
 rm ../../../DAM/Debug/configuraciones/configuracion.txt
 cp configuracion.txt ../../../DAM/Debug/configuraciones
 cd 
-cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/CPU/prueba2
+cd workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/CPU/prueba2
 rm ../../../CPU/Debug/configuraciones/configuracion.txt
 cp configuracion.txt ../../../CPU/Debug/configuraciones
 cd 
-cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/SAFA/prueba2
+cd workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/SAFA/prueba2
 rm ../../../S-AFA/Debug/configuraciones/configuracion.txt
 cp configuracion.txt ../../../S-AFA/Debug/configuraciones
 cd 
-cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/MDJ/prueba2
+cd workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/MDJ/prueba2
 rm ../../../MDJ/Debug/configuraciones/configuracion.txt
 cp configuracion.txt ../../../MDJ/Debug/configuraciones
 cd 
-cd /workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/FM9/prueba2
+cd workspace/tp-2018-2c-5-fant-sticos/setupConfiguraciones/FM9/prueba2
 rm ../../../FM9/Debug/configuraciones/configuracion.txt
 cp configuracion.txt ../../../FM9/Debug/configuraciones
 cd 
-cd /workspace/tp-2018-2c-5-fant-sticos
+cd workspace/tp-2018-2c-5-fant-sticos
