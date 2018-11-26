d63c6e2e54f5641266645f6efe532aa51eba10cc
florlipchak <florencia.lipchak@gmail.com>
2018-11-25 23:55:56 -0300

    paginada e invertida

--- FM9/src/segmentacionPag.c
+++ FM9/src/segmentacionPag.c
@@ -339,33 +339,18 @@ int asignarDatosSegPag(int IdDTB, char* nombreArchivo, int numeroLinea, char* da
 		signalMutex(&mutexListaPaginas);
 		int desplazamientoPagina = pagina->marco * tamanioPagina * tamanioLinea; //Porque el tamanioPagina esta en lineas
 		int desplazamientoLinea = lineaDentroDeLaPagina * tamanioLinea;
-		char* lineaConBasura = asignarMemoria(tamanioLinea);
-		memcpy(lineaConBasura, storage + desplazamientoPagina + desplazamientoLinea, tamanioLinea);
-		char** lineaSinBasura = string_split(lineaConBasura, "\n");
-		char* lineaPosta;
-		if(lineaSinBasura[0] == NULL)
-			lineaPosta = string_new();
-		else{
-			lineaPosta = malloc(strlen(lineaSinBasura[0]));
-			memcpy(lineaPosta, lineaSinBasura[0], strlen(lineaSinBasura[0]));
-		}
-		if((strlen(lineaPosta) + strlen(datos) + 2) < tamanioLinea){ //Lo que ya estaba, los datos nuevos, el /n y el espacio en el medio
+		if((strlen(datos) + 1) < tamanioLinea){ //Lo que ya estaba, los datos nuevos, el /n y el espacio en el medio
 			//Se puede escribir
-			string_append_with_format(&lineaPosta, " %s\n", datos);
-			log_debug("Linea resultante de la asignación: %s", lineaPosta);
+			log_debug("Linea que va a asignarse: %s", datos);
 			char* lineaAGuardar = malloc(tamanioLinea);
-			memcpy(lineaAGuardar, lineaPosta, strlen(lineaPosta) + 1);
+			memcpy(lineaAGuardar, datos, strlen(datos) + 1);
 			string_trim(&lineaAGuardar);
 			memcpy(storage + desplazamientoPagina + desplazamientoLinea, lineaAGuardar, tamanioLinea);
-			freeLineasBasura(lineaSinBasura, lineaConBasura);
-			free(lineaPosta);
 			free(lineaAGuardar);
 			log_debug(logger, "Asignados datos con exito");
 			return 0;
 
 		}else{
-			freeLineasBasura(lineaSinBasura, lineaConBasura);
-			free(lineaPosta);
 			log_error(logger, "No hay suficiente espacio en la linea %d del archivo %s", (numeroLinea+1), nombreArchivo);
 			return ESPACIO_INSUFICIENTE_EN_FM9; //ERROR
 		}
